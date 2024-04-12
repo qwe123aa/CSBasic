63d727cc59a8fbb48e307ff14d884f52d60a060e
thdud__ <sylim1114y@gmail.com>
2024-04-12 10:10:18 +0900

    #7 int, double 자료형 최대최소 출력 지수표기법 주의

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -23,6 +23,11 @@ namespace CSBasic
             Console.WriteLine(a+(long)b);
             Console.WriteLine((long)a + (long)b);
 
+            //자료형 최소, 최대 출력하기
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(double.MinValue);
+            Console.WriteLine(double.MaxValue);
         }
     }
 }
